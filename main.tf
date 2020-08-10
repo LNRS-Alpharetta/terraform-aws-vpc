ab126e4df77c9df68334c9123ae3b1448db24113
Jack Coleman <jack.coleman@lexisnexisrisk.com>
2020-08-10 14:09:47 -0400

    Update main.tf
    revert

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ locals {
 resource "aws_vpc" "this" {
   count = var.create_vpc ? 1 : 0
 
-  cidr_block                       = var.subnet_prefix
+  cidr_block                       = var.cidr
   instance_tenancy                 = var.instance_tenancy
   enable_dns_hostnames             = var.enable_dns_hostnames
   enable_dns_support               = var.enable_dns_support
